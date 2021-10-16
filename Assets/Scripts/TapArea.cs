c67155e1e2c7f3e8fde8061748a0c68131e2892a
Khalish-Litsch <khalish260303@gmail.com>
2021-10-16 08:23:40 +0800

    menambahkan game yang berjudul incremental game

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
